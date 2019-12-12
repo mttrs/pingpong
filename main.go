a5d7ea88e5fe79244cfc1f85355873c5159a624d
Mitsuteru Sawa <mttrs@users.noreply.github.com>
2019-12-12 08:23:50 +0000

    Add a wait handler

--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong\n")
 }
 
+func waitHandler(w http.ResponseWriter, r *http.Request) {
+	waitTime := 3 * time.Second
+	fmt.Println("waiting for", waitTime)
+	time.Sleep(waitTime)
+	fmt.Fprintf(w, "Thanks for waiting\n")
+}
+
 func main() {
+	http.HandleFunc("/wait", waitHandler)
 	http.HandleFunc("/", handler)
 
 	port := os.Getenv("PORT")
