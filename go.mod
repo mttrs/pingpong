1c1fb5535fbd472434118b119ac18ce1f7c9ebb0
Mitsuteru Sawa <mttrs@users.noreply.github.com>
2020-01-07 05:54:31 +0000

    Add pq

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@
 module github.com/mttrs/pingpong
 
 go 1.13
+
+require github.com/lib/pq v1.3.0
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+_	"github.com/lib/pq"
 	"net/http"
 	"os"
 	"time"
 )
+
+var db *sql.DB
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong\n")
@@ -13,19 +18,48 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func waitHandler(w http.ResponseWriter, r *http.Request) {
 	waitTime := 2 * time.Second
-	fmt.Println("waiting for", waitTime)
+	log.Println("waiting for", waitTime)
 	time.Sleep(waitTime)
+
 	fmt.Fprintf(w, "Thanks for waiting\n")
 }
 
+func sqlHandler(w http.ResponseWriter, r *http.Request) {
+	var result string
+	err := db.QueryRow("SELECT 1").Scan(&result)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, result)
+}
+
+func DBSetup(){
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		connStr = "postgres://localhost:5432/postgres?sslmode=disable"
+	}
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+}
+
 func main() {
+	DBSetup()
+
 	http.HandleFunc("/wait", waitHandler)
+	http.HandleFunc("/list", sqlHandler)
 	http.HandleFunc("/", handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	fmt.Println("Running on:", port, "...")
+	log.Println("Running on:", port, "...")
 	http.ListenAndServe(":"+port, nil)
 }
